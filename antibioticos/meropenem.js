de2c295d91edea5770e5ee3afd01cdede0a75800
Marrone-Lorentz <lomarrone@gmail.com>
2026-06-05 18:57:20 -0300

    Create meropenem.js

---
+++ antibioticos/meropenem.js
@@ -0,0 +1,21 @@
+window.bancoDeAntibioticos["meropenem"] = {
+    nome: "Meropenem",
+    avaliar: function(peso, clcr, dialise) {
+        if (dialise) {
+            return "Dose: 500 mg a cada 24h.\nAdministrar a dose após a sessão nos dias de hemodiálise.";
+        }
+        if (clcr >= 50) {
+            return "Dose: 1g a 2g a cada 8h.\nDiluição: 100 mL SF 0,9%.\nTempo de Infusão: 3 horas (infusão estendida).";
+        }
+        if (clcr >= 26 && clcr < 50) {
+            return "Dose: 1g a cada 12h.\nDiluição: 100 mL SF 0,9%.\nTempo de Infusão: 3 horas.";
+        }
+        if (clcr >= 10 && clcr <= 25) {
+            return "Dose: 500 mg a cada 12h.";
+        }
+        if (clcr < 10) {
+            return "Dose: 500 mg a cada 24h.";
+        }
+        return "Dados clínicos insuficientes para gerar a recomendação.";
+    }
+};
