22a283fda8dd51ee8a6bf791dab8565a4cb88636
Marrone-Lorentz <lomarrone@gmail.com>
2026-06-05 22:56:06 -0300

    Update amoxicicilina+clavulanato.js

--- antibioticos/amoxicicilina+clavulanato.js
+++ antibioticos/amoxicicilina+clavulanato.js
@@ -9,8 +9,8 @@ window.bancoDeAntibioticos["amoxicicilina+clavulanato"] = {
         let dose40 = peso * 30;
         let diluicao = dose40 / 12;
 
-        return `
-            <div class="div-tabela">
+        if (clcr >= 50 && peso >= 40) {
+            return <div class="div-tabela">
                 <h5 style="${estiloTitulo}">Acima de 40 Quilos</h5>
                 <table style="${estiloTabela}">
                     <thead>
@@ -24,13 +24,104 @@ window.bancoDeAntibioticos["amoxicicilina+clavulanato"] = {
                         <tr>
                             <td style="${estiloCelula}">1,2g EV 8/8h</td>
                             <td style="${estiloCelula}">SF 100ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        }
+        if (clcr < 50 && clcr >=10 && peso >= 40) {
+            return <div class="div-tabela">
+                <h5 style="${estiloTitulo}">Acima de 40 Quilos</h5>
+                <table style="${estiloTabela}">
+                    <thead>
+                        <tr>
+                            <th style="${estiloHeader}"> </th>
+                            <th style="${estiloHeader}">Posologia</th>
+                            <th style="${estiloHeader}">Diluição</th>
+                            <th style="${estiloHeader}">Tempo de infusão</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style="${estiloCelula}">Ataque</td>
+                            <td style="${estiloCelula}">1,2g EV imediato</td>
+                            <td style="${estiloCelula}">SF 100ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                        <tr>
+                            <td style="${estiloCelula}">Manutenção</td>
+                            <td style="${estiloCelula}">600mg EV 12/12h</td>
+                            <td style="${estiloCelula}">SF 50ml</td>
                             <td style="${estiloCelula}">30-40 minutos</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-
-            <div class="div-tabela">
+        }
+        if (clcr < 10 && peso >= 40) {
+            return <div class="div-tabela">
+                <h5 style="${estiloTitulo}">Acima de 40 Quilos</h5>
+                <table style="${estiloTabela}">
+                    <thead>
+                        <tr>
+                            <th style="${estiloHeader}"> </th>
+                            <th style="${estiloHeader}">Posologia</th>
+                            <th style="${estiloHeader}">Diluição</th>
+                            <th style="${estiloHeader}">Tempo de infusão</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style="${estiloCelula}">Ataque</td>
+                            <td style="${estiloCelula}">1,2g EV imediato</td>
+                            <td style="${estiloCelula}">SF 100ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                        <tr>
+                            <td style="${estiloCelula}">Manutenção</td>
+                            <td style="${estiloCelula}">600mg EV 24/24h</td>
+                            <td style="${estiloCelula}">SF 50ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        }
+        if (dialise) {
+            return <div class="div-tabela">
+                <h5 style="${estiloTitulo}">Acima de 40 Quilos</h5>
+                <table style="${estiloTabela}">
+                    <thead>
+                        <tr>
+                            <th style="${estiloHeader}"> </th>
+                            <th style="${estiloHeader}">Posologia</th>
+                            <th style="${estiloHeader}">Diluição</th>
+                            <th style="${estiloHeader}">Tempo de infusão</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style="${estiloCelula}">Ataque</td>
+                            <td style="${estiloCelula}">1,2g EV imediato</td>
+                            <td style="${estiloCelula}">SF 100ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                        <tr>
+                            <td style="${estiloCelula}">Manutenção</td>
+                            <td style="${estiloCelula}">600mg EV 24/24h</td>
+                            <td style="${estiloCelula}">SF 50ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                        <tr>
+                            <td style="${estiloCelula}">Fazer dose extra após diálise</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        }
+        if (clcr >= 50 && peso < 40) {
+            return <div class="div-tabela">
                 <h5 style="${estiloTitulo}">Abaixo de 40 Quilos</h5>
                 <table style="${estiloTabela}">
                     <thead>
@@ -44,11 +135,78 @@ window.bancoDeAntibioticos["amoxicicilina+clavulanato"] = {
                         <tr>
                             <td style="${estiloCelula}">${dose40} miligramas 8/8h</td>
                             <td style="${estiloCelula}">SF ${diluicao}ml</td>
-                            <td style="${estiloCelula}">30-40 minutos</td>
+                            <td style="${estiloCelula}">30 minutos</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-        `;
+        }
+        if (clcr < 50 && clcr >= 10 && peso < 40) {
+            return <div class="div-tabela">
+                <h5 style="${estiloTitulo}">Abaixo de 40 Quilos</h5>
+                <table style="${estiloTabela}">
+                    <thead>
+                        <tr>
+                            <th style="${estiloHeader}">Posologia</th>
+                            <th style="${estiloHeader}">Diluição</th>
+                            <th style="${estiloHeader}">Tempo de infusão</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style="${estiloCelula}">${dose40} miligramas 12/12h</td>
+                            <td style="${estiloCelula}">SF ${diluicao}ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        }
+        if (clcr < 10 && peso < 40) {
+            return <div class="div-tabela">
+                <h5 style="${estiloTitulo}">Abaixo de 40 Quilos</h5>
+                <table style="${estiloTabela}">
+                    <thead>
+                        <tr>
+                            <th style="${estiloHeader}">Posologia</th>
+                            <th style="${estiloHeader}">Diluição</th>
+                            <th style="${estiloHeader}">Tempo de infusão</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style="${estiloCelula}">${dose40} miligramas 24/24h</td>
+                            <td style="${estiloCelula}">SF ${diluicao}ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        }
+        if (dialise) {
+            return <div class="div-tabela">
+                <h5 style="${estiloTitulo}">Abaixo de 40 Quilos</h5>
+                <table style="${estiloTabela}">
+                    <thead>
+                        <tr>
+                            <th style="${estiloHeader}">Posologia</th>
+                            <th style="${estiloHeader}">Diluição</th>
+                            <th style="${estiloHeader}">Tempo de infusão</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style="${estiloCelula}">${dose40} miligramas 24/24h</td>
+                            <td style="${estiloCelula}">SF ${diluicao}ml</td>
+                            <td style="${estiloCelula}">30 minutos</td>
+                        </tr>
+                        <tr>
+                            <td style="${estiloCelula}">Fazer dose extra após diálise</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        }
+        return "Dados clínicos insuficientes para gerar a recomendação.";
     }
 };
