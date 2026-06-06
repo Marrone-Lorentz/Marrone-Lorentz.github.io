4fb7e2fa1f4f263d38c9509fcfe0e34ca95f2f06
Marrone-Lorentz <lomarrone@gmail.com>
2026-06-05 23:31:21 -0300

    Update amoxicicilina+clavulanato.js

--- antibioticos/amoxicicilina+clavulanato.js
+++ antibioticos/amoxicicilina+clavulanato.js
@@ -121,7 +121,7 @@ window.bancoDeAntibioticos["amoxicicilina+clavulanato"] = {
                             <td style="${estiloCelula}">30 minutos</td>
                         </tr>
                         <tr>
-                            <td colspan="3" style="${estiloCelula}">Fazer dose extra após diálise</td>
+                            <td colspan="4" style="${estiloCelula}">Fazer dose extra após diálise</td>
                         </tr>
                     </tbody>
                 </table>
